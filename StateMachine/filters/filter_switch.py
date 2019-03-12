ab74b71575eb992473fde42242f4395eb6ffaa7e
skmccormack <35875323+skmccormack@users.noreply.github.com>
2019-03-12 14:15:52 -0400

    Create filter_switch.py

---
+++ StateMachine/filters/filter_switch.py
@@ -0,0 +1,10 @@
+# StateMachine/filters/filter_switch.py
+# State Machine pattern using 'if' statements
+# to determine the next state.
+import string, sys
+sys.path += ['../StateMachine', '../button']
+from State import State
+from StateMachine import StateMachine
+from ButtonPress import ButtonPress
+# A different subclass for each state:
+class Clipping
