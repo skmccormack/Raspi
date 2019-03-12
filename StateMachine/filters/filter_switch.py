47ea4a6cd22f9ec45ec00088b7ff08619e5dda84
skmccormack <35875323+skmccormack@users.noreply.github.com>
2019-03-12 14:22:17 -0400

    Update filter_switch.py

--- StateMachine/filters/filter_switch.py
+++ StateMachine/filters/filter_switch.py
@@ -7,4 +7,52 @@ from State import State
 from StateMachine import StateMachine
 from ButtonPress import ButtonPress
 # A different subclass for each state:
-class Clipping
+class Clipping(State):
+  def run(self):
+    pot = ValueChange().pot.value
+    data_out = {pot, addr, pot.value}
+  def next(self, input):
+    if input == 1:
+      return Filters.LowPass
+    return Filters.Clipping
+
+class LowPass(State):
+  def run(self):
+    pot = ValueChange().pot.value
+    data_out = {pot, addr, pot.value}
+  def next(self, input):
+    if input == 1:
+      return Filters.HighPass
+    return Filters.LowPass
+
+class HighPass(State):
+  def run(self):
+    pot = ValueChange().pot.value
+    data_out = {pot, addr, pot.value}
+  def next(self, input):
+    if input == 1:
+      return Filters.BandPass
+    return Filters.HighPass
+  
+class BandPass(State):
+  def run(self):
+    pot = ValueChange().pot.value
+    data_out = {pot, addr, pot.value}
+  def next(self, input):
+    if input == 1:
+      return Filters.BandStop
+    return Filters.BandPass
+  
+class BandStop(State):
+  def run(self):
+    pot = ValueChange().pot.value
+    data_out = {pot, addr, pot.value}
+  def next(self, input):
+    if input == 1:
+      return Filters.Clipping
+    return Filters.BandStop
+  
+class Filters(StateMachine):
+  def __init__(self):
+    #Intial State
+    StateMachine.__init__(self.Filters.Clipping)
