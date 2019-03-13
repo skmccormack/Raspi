3637bc882b1ed718f64fcfba4c00edd8c175ff72
skmccormack <35875323+skmccormack@users.noreply.github.com>
2019-03-13 18:55:32 -0400

    Update filter_switch.py

--- StateMachine/filters/filter_switch.py
+++ StateMachine/filters/filter_switch.py
@@ -6,11 +6,41 @@ sys.path += ['../StateMachine', '../button']
 from State import State
 from StateMachine import StateMachine
 from ButtonPress import ButtonPress
+
+#Devices
+class Devices:
+  def __init__(self, value):
+    self.value = value
+clip = Devices(100)  #initial value
+lpc = Devices(100)  #initial value
+lpq = Devices(100)  #initial value
+hpc = Devices(100)  #initial value
+hpq = Devices(100)  #initial value
+bpc = Devices(100)  #initial value
+bpq = Devices(100)  #initial value
+bsc = Devices(100)  #initial value
+bsq = Devices(100)  #initial value
+
+#Function to change pot value with vertical buttons
+def ValueChangeV(value):
+  if ButtonPress.up == 1:
+    value = value + inc
+  if ButtonPress.down == 1:
+    value = value - inc
+    
+#Function to change pot value with horizontal buttons
+def ValueChangeH(value):
+  if ButtonPress.right == 1:
+    value = value + inc
+  if ButtonPress.left == 1:
+    value = value - inc
+
 # A different subclass for each state:
 class Clipping(State):
   def run(self):
-    pot = ValueChange().pot.value
-    data_out = {pot, addr, pot.value}
+    clip.value = ValueChangeV(clip.value)
+    data_out = {addr, 0b00, clip.value}    #NEEDS FIXING
+    #turn on clipping device cs, set MOSI to data out, turn off cs
   def next(self, input):
     if input == 1:
       return Filters.lowpass
@@ -18,8 +48,12 @@ class Clipping(State):
 
 class LowPass(State):
   def run(self):
-    pot = ValueChange().pot.value
-    data_out = {pot, addr, pot.value}
+    lpq.value = ValueChangeV(lpq.value)
+    data_out = {addr, 0b00, lpq.value}   #NEEDS FIXING
+    #turn on lpq device cs, set MOSI to data out, turn off cs
+    lpc.value = ValueChangeH(lpc.value)
+    data_out = {addr, 0b00, lpc.value}   #NEEDS FIXING
+    #turn on lpc device cs, set MOSI to data out, turn off cs
   def next(self, input):
     if input == 1:
       return Filters.highpass
@@ -27,8 +61,12 @@ class LowPass(State):
 
 class HighPass(State):
   def run(self):
-    pot = ValueChange().pot.value
-    data_out = {pot, addr, pot.value}
+    hpq.value = ValueChangeV(hpq.value)
+    data_out = {addr, 0b00, hpq.value}   #NEEDS FIXING
+    #turn on hpq device cs, set MOSI to data out, turn off cs
+    hpc.value = ValueChangeH(hpc.value)
+    data_out = {addr, 0b00, hpc.value}   #NEEDS FIXING
+    #turn on hpc device cs, set MOSI to data out, turn off cs
   def next(self, input):
     if input == 1:
       return Filters.bandpass
@@ -36,8 +74,12 @@ class HighPass(State):
   
 class BandPass(State):
   def run(self):
-    pot = ValueChange().pot.value
-    data_out = {pot, addr, pot.value}
+    bpq.value = ValueChangeV(bpq.value)
+    data_out = {addr, 0b00, bpq.value}   #NEEDS FIXING
+    #turn on bpq device cs, set MOSI to data out, turn off cs
+    bpc.value = ValueChangeH(bpc.value)
+    data_out = {addr, 0b00, bpc.value}   #NEEDS FIXING
+    #turn on bpc device cs, set MOSI to data out, turn off cs
   def next(self, input):
     if input == 1:
       return Filters.bandstop
@@ -45,8 +87,12 @@ class BandPass(State):
   
 class BandStop(State):
   def run(self):
-    pot = ValueChange().pot.value
-    data_out = {pot, addr, pot.value}
+    bsq.value = ValueChangeV(bsq.value)
+    data_out = {addr, 0b00, bsq.value}   #NEEDS FIXING
+    #turn on bsq device cs, set MOSI to data out, turn off cs
+    hpc.value = ValueChangeH(bsc.value)
+    data_out = {addr, 0b00, bsc.value}   #NEEDS FIXING
+    #turn on bsc device cs, set MOSI to data out, turn off cs
   def next(self, input):
     if input == 1:
       return Filters.Clipping
