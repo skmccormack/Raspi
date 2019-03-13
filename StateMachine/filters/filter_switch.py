f50f2389f167ed46343dbcd767e931e301e93036
skmccormack <35875323+skmccormack@users.noreply.github.com>
2019-03-13 19:06:44 -0400

    Update filter_switch.py

--- StateMachine/filters/filter_switch.py
+++ StateMachine/filters/filter_switch.py
@@ -37,64 +37,89 @@ def ValueChangeH(value):
 
 # A different subclass for each state:
 class Clipping(State):
-  def run(self):
-    clip.value = ValueChangeV(clip.value)
-    data_out = {addr, 0b00, clip.value}    #NEEDS FIXING
-    #turn on clipping device cs, set MOSI to data out, turn off cs
+  def run(self, input):
+    if input == ButtonPress.on:
+      #Give value to swtich dev
+    if input == ButtonPress.off:
+      #Give value to swtich dev
+    if #switch dev on:
+      clip.value = ValueChangeV(clip.value)
+      data_out = {addr, 0b00, clip.value}    #NEEDS FIXING
+      #turn on clipping device cs, set MOSI to data out, turn off cs
   def next(self, input):
-    if input == ButtonPress.select:
+    if input == ButtonPress.sel:
       return Filters.lowpass
     return Filters.clipping
 
 class LowPass(State):
-  def run(self):
-    lpq.value = ValueChangeV(lpq.value)
-    data_out = {addr, 0b00, lpq.value}   #NEEDS FIXING
-    #turn on lpq device cs, set MOSI to data out, turn off cs
-    lpc.value = ValueChangeH(lpc.value)
-    data_out = {addr, 0b00, lpc.value}   #NEEDS FIXING
-    #turn on lpc device cs, set MOSI to data out, turn off cs
+  def run(self, input):
+    if input == ButtonPress.on:
+      #Give value to swtich dev
+    if input == ButtonPress.off:
+      #Give value to swtich dev
+    if #switch dev on:
+      lpq.value = ValueChangeV(lpq.value)
+      data_out = {addr, 0b00, lpq.value}   #NEEDS FIXING
+      #turn on lpq device cs, set MOSI to data out, turn off cs
+      lpc.value = ValueChangeH(lpc.value)
+      data_out = {addr, 0b00, lpc.value}   #NEEDS FIXING
+      #turn on lpc device cs, set MOSI to data out, turn off cs
   def next(self, input):
-    if input == ButtonPress.select:
+    if input == ButtonPress.sel:
       return Filters.highpass
     return Filters.lowpass
 
 class HighPass(State):
-  def run(self):
-    hpq.value = ValueChangeV(hpq.value)
-    data_out = {addr, 0b00, hpq.value}   #NEEDS FIXING
-    #turn on hpq device cs, set MOSI to data out, turn off cs
-    hpc.value = ValueChangeH(hpc.value)
-    data_out = {addr, 0b00, hpc.value}   #NEEDS FIXING
-    #turn on hpc device cs, set MOSI to data out, turn off cs
+  def run(self, input):
+    if input == ButtonPress.on:
+      #Give value to swtich dev
+    if input == ButtonPress.off:
+      #Give value to swtich dev
+    if #switch dev on:
+      hpq.value = ValueChangeV(hpq.value)
+      data_out = {addr, 0b00, hpq.value}   #NEEDS FIXING
+      #turn on hpq device cs, set MOSI to data out, turn off cs
+      hpc.value = ValueChangeH(hpc.value)
+      data_out = {addr, 0b00, hpc.value}   #NEEDS FIXING
+      #turn on hpc device cs, set MOSI to data out, turn off cs
   def next(self, input):
-    if input == ButtonPress.select:
+    if input == ButtonPress.sel:
       return Filters.bandpass
     return Filters.highpass
   
 class BandPass(State):
-  def run(self):
-    bpq.value = ValueChangeV(bpq.value)
-    data_out = {addr, 0b00, bpq.value}   #NEEDS FIXING
-    #turn on bpq device cs, set MOSI to data out, turn off cs
-    bpc.value = ValueChangeH(bpc.value)
-    data_out = {addr, 0b00, bpc.value}   #NEEDS FIXING
-    #turn on bpc device cs, set MOSI to data out, turn off cs
+  def run(self, input):
+    if input == ButtonPress.on:
+      #Give value to swtich dev
+    if input == ButtonPress.off:
+      #Give value to swtich dev
+    if #switch dev on:
+      bpq.value = ValueChangeV(bpq.value)
+      data_out = {addr, 0b00, bpq.value}   #NEEDS FIXING
+      #turn on bpq device cs, set MOSI to data out, turn off cs
+      bpc.value = ValueChangeH(bpc.value)
+      data_out = {addr, 0b00, bpc.value}   #NEEDS FIXING
+      #turn on bpc device cs, set MOSI to data out, turn off cs
   def next(self, input):
-    if input == ButtonPress.select:
+    if input == ButtonPress.sel:
       return Filters.bandstop
     return Filters.bandpass
   
 class BandStop(State):
-  def run(self):
-    bsq.value = ValueChangeV(bsq.value)
-    data_out = {addr, 0b00, bsq.value}   #NEEDS FIXING
-    #turn on bsq device cs, set MOSI to data out, turn off cs
-    hpc.value = ValueChangeH(bsc.value)
-    data_out = {addr, 0b00, bsc.value}   #NEEDS FIXING
-    #turn on bsc device cs, set MOSI to data out, turn off cs
+  def run(self, input):
+    if input == ButtonPress.on:
+      #Give value to swtich dev
+    if input == ButtonPress.off:
+      #Give value to swtich dev
+    if #switch dev on:
+      bsq.value = ValueChangeV(bsq.value)
+      data_out = {addr, 0b00, bsq.value}   #NEEDS FIXING
+      #turn on bsq device cs, set MOSI to data out, turn off cs
+      hpc.value = ValueChangeH(bsc.value)
+      data_out = {addr, 0b00, bsc.value}   #NEEDS FIXING
+      #turn on bsc device cs, set MOSI to data out, turn off cs
   def next(self, input):
-    if input == ButtonPress.select:
+    if input == ButtonPress.sel:
       return Filters.Clipping
     return Filters.bandstop
   
