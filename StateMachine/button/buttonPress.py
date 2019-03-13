a9ed41220ce22b1accb758ef8394c0832af4df48
skmccormack <35875323+skmccormack@users.noreply.github.com>
2019-03-13 18:22:52 -0400

    Update buttonPress.py

--- StateMachine/button/buttonPress.py
+++ StateMachine/button/buttonPress.py
@@ -2,3 +2,12 @@ class ButtonPress:
   def __init__(self, action):
     self.action = action
   def __str__(self): return self.action
+
+# Static fields; an enumeration of instances:
+ButtonPress.on = ButtonPress("On")
+ButtonPress.off = ButtonPress("Off")
+ButtonPress.sel = ButtonPress("Select")
+ButtonPress.left = ButtonPress("Left")
+ButtonPress.right = ButtonPress("Right")
+ButtonPress.up = ButtonPress("Up")
+ButtonPress.down = ButtonPress("Down")
