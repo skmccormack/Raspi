8cd45049d4af5b2b855875c9e575e3d5f7006beb
skmccormack <35875323+skmccormack@users.noreply.github.com>
2019-03-13 19:13:55 -0400

    Update State.py

--- StateMachine/State.py
+++ StateMachine/State.py
@@ -3,3 +3,8 @@ class State:
       assert 0, "run not implemented"
   def next(self, input):
       assert 0, "next not implemented"
+  def runAll(self, inputs):
+      for i in inputs:
+          print(i)
+          self.currentState = self.currentState.next(i)
+          self.currentState.run()
