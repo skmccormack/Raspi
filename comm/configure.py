6c2eb6a9012581615a42e40455010c23ccd17bc1
skmccormack <35875323+skmccormack@users.noreply.github.com>
2019-03-20 18:17:38 -0400

    Update configure.py

--- comm/configure.py
+++ comm/configure.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import time
 
 #To set numbing system
 GPIO.setmode(GPIO.BOARD)
@@ -11,23 +12,47 @@ GPIO.setmode(GPIO.BOARD)
 
 #Set up IOs
 #Switch CSs
-LPSw = GPIO.setup(40, GPIO.OUT, initial=GPIO.LOW)   #Low Pass Bypass
-HPSw = GPIO.setup(38, GPIO.OUT, initial=GPIO.LOW)   #High Pass Bypass
-BPSw = GPIO.setup(37, GPIO.OUT, initial=GPIO.LOW)   #Band Pass Bypass
-BSSw = GPIO.setup(36, GPIO.OUT, initial=GPIO.LOW)   #Band Stop Bypass
-ClipSw = GPIO.setup(35, GPIO.OUT, initial=GPIO.LOW)   #Clipping Bypass
+LPSw = 40
+GPIO.setup(40, GPIO.OUT, initial=GPIO.LOW)   #Low Pass Bypass
+HPSw = 38
+GPIO.setup(38, GPIO.OUT, initial=GPIO.LOW)   #High Pass Bypass
+BPSw = 37
+GPIO.setup(37, GPIO.OUT, initial=GPIO.LOW)   #Band Pass Bypass
+BSSw = 36
+GPIO.setup(36, GPIO.OUT, initial=GPIO.LOW)   #Band Stop Bypass
+ClipSw = 35
+GPIO.setup(35, GPIO.OUT, initial=GPIO.LOW)   #Clipping Bypass
 #Digital Potentiometer CSs
-LPc = GPIO.setup(33, GPIO.OUT, initial=GPIO.LOW)   #LP cutoff (dual)
-LPq = GPIO.setup(32, GPIO.OUT, initial=GPIO.LOW)   #LP q (single)
-HPc = GPIO.setup(31, GPIO.OUT, initial=GPIO.LOW)   #HP cutoff (dual)
-HPq = GPIO.setup(29, GPIO.OUT, initial=GPIO.LOW)   #HP q (single)
-BPc = GPIO.setup(18, GPIO.OUT, initial=GPIO.LOW)   #BP cutoff (dual)
-BPq = GPIO.setup(16, GPIO.OUT, initial=GPIO.LOW)   #BP q (single)
-BSc = GPIO.setup(15, GPIO.OUT, initial=GPIO.LOW)   #BS cutoff (dual)
-BSq = GPIO.setup(12, GPIO.OUT, initial=GPIO.LOW)   #BS q (single)
-Clip = GPIO.setup(13, GPIO.OUT, initial=GPIO.LOW)   #Clipping (dual)
+LPc = 33
+GPIO.setup(33, GPIO.OUT, initial=GPIO.LOW)   #LP cutoff (dual)
+LPq = 32
+GPIO.setup(32, GPIO.OUT, initial=GPIO.LOW)   #LP q (single)
+HPc = 31
+GPIO.setup(31, GPIO.OUT, initial=GPIO.LOW)   #HP cutoff (dual)
+HPq = 29
+GPIO.setup(29, GPIO.OUT, initial=GPIO.LOW)   #HP q (single)
+BPc = 18
+GPIO.setup(18, GPIO.OUT, initial=GPIO.LOW)   #BP cutoff (dual)
+BPq = 16
+GPIO.setup(16, GPIO.OUT, initial=GPIO.LOW)   #BP q (single)
+BSc = 15
+GPIO.setup(15, GPIO.OUT, initial=GPIO.LOW)   #BS cutoff (dual)
+BSq = 12
+GPIO.setup(12, GPIO.OUT, initial=GPIO.LOW)   #BS q (single)
+Clip = 13
+GPIO.setup(13, GPIO.OUT, initial=GPIO.LOW)   #Clipping (dual)
 
-
-
-#Use this outside of while loop for restart pin assignments
-#GPIO.cleanup()
+def comtodual(pot, value):
+  GPIO.output(pot, 1)
+  time.sleep(0.00000006)
+  #MOSI line command
+  
+def comtosing(pot, value):
+  GPIO.output(pot, 1)
+  time.sleep(0.00000006)
+  #MOSI line command
+  
+def comtosw(pot, value):
+  GPIO.output(pot, 1)
+  time.sleep()
+  #MOSI line command
