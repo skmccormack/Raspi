577fac33fde375c7f1f9ac5641a3b36411c4bbb4
skmccormack <35875323+skmccormack@users.noreply.github.com>
2019-03-24 05:30:25 -0400

    Update comtodual.py

--- comm/comtodual.py
+++ comm/comtodual.py
@@ -1,2 +1,21 @@
-def comtodual(pot, value):
+def increment(value, pottype):
+    if value < 250:
+        value += 1
+        if pottype == 1:
+            #Write to dual pot the new value
+        else:
+            #Write to singular pot the new value
 
+def decrement(value, pottype):
+    if value > 0:
+        value -= 1
+        if pottype == 1:
+            #Write to dual pot the new value
+        else:
+            #Write to singular pot the new value
+
+def comtosw(dev, value)
+    if value == 1:
+        #Turn off specified bypass switch (Filter = ON)
+    if value == 0:
+        #Turn on specified bypass switch (Filter = OFF)
